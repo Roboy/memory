5052d862a2d23f12aa563e8ef5b109babb71a384
AlexMoroz <oleksii.moroz@tum.de>
2017-06-01 18:53:25 +0200

    change neo4j address

--- src/main/java/roboy_memory/Config.java
+++ src/main/java/roboy_memory/Config.java
@@ -8,7 +8,7 @@ class Config {
     //ROS
     final static String ROS_MASTER_ADDRESS = "http://localhost:11311/";
     //Neo4j
-    final static String NEO4J_ADDRESS = "http://localhost:7474/";
+    final static String NEO4J_ADDRESS = "bolt://85.10.197.57:7687";
     final static String NEO4J_USERNAME = "memory";
     final static String NEO4J_PASSWORD = "*************"; //TODO: don't commit password to the repo
 
