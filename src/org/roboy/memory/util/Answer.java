1c4198c9ae3f3f04f62046d42bcbe9c665f3fe64
AlexMoroz <oleksii.moroz@tum.de>
2017-08-24 18:32:05 +0200

    fix answer format

--- src/org/roboy/memory/util/Answer.java
+++ src/org/roboy/memory/util/Answer.java
@@ -12,13 +12,13 @@ public class Answer {
 
 
     public static String ok() {
-        String result = "{status:\"OK\"}";
+        String result = "{\"status\":\"OK\"}";
         logger.info(result);
         return result;
     }
 
     public static String error(String message) {
-        String result = "{status:\"FAIL\", message:\""+message+"\"}";
+        String result = "{\"status\":\"FAIL\", \"message\":\""+message+"\"}";
         logger.warning(result);
         return result;
     }
