8a824c70597062532f517ac1a9e21d3776423add
Wagram Airian <vagram.airiian@tum.de>
2017-09-20 19:05:56 +0300

    Updated LABELS dict

--- src/org/roboy/memory/util/Dictionary.java
+++ src/org/roboy/memory/util/Dictionary.java
@@ -4,6 +4,6 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class Dictionary {
-    public static final HashSet<String> LABEL_VALUES = new HashSet<String>(Arrays.asList("Person","Robot","Company","University","City","Country","Hobby","Occupation","Object"));
+    public static final HashSet<String> LABEL_VALUES = new HashSet<String>(Arrays.asList("Person","Robot","Company","University","City","Country","Hobby","Occupation","Object","Location","Organization"));
     public static final HashSet<String> RELATIONSHIP_VALUES = new HashSet<String> (Arrays.asList("FRIEND_OF","LIVE_IN","FROM","WORK_FOR","STUDY_AT","MEMBER_OF","HAS_HOBBY","KNOW","IS","PART_OF","IS_IN" ));
 }
