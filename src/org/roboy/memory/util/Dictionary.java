09cf77906f89b4fc032890f2ba4cb93fb847ce77
Wagram Airian <vagram.airiian@tum.de>
2017-12-20 21:47:48 +0100

    Fixes and Workarounds. +Questions not in onthology

--- src/org/roboy/memory/util/Dictionary.java
+++ src/org/roboy/memory/util/Dictionary.java
@@ -5,5 +5,5 @@ import java.util.HashSet;
 
 public class Dictionary {
     public static final HashSet<String> LABEL_VALUES = new HashSet<String>(Arrays.asList("Person","Robot","Company","University","City","Country","Hobby","Occupation","Object","Location","Organization"));
-    public static final HashSet<String> RELATIONSHIP_VALUES = new HashSet<String> (Arrays.asList("FRIEND_OF","LIVE_IN","FROM","WORK_FOR","STUDY_AT","MEMBER_OF","HAS_HOBBY","KNOW","IS","PART_OF","IS_IN" ));
+    public static final HashSet<String> RELATIONSHIP_VALUES = new HashSet<String> (Arrays.asList("FRIEND_OF","LIVE_IN","FROM","WORK_FOR","STUDY_AT","MEMBER_OF","HAS_HOBBY","KNOW","IS","PART_OF","IS_IN","OCCUPIED_AS" ));
 }
