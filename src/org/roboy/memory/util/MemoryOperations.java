9552eeb76a4979d6ba44e8ff967ccff4114aaaa6
Jayce <luoyizhong98+git@gmail.com>
2018-06-08 10:48:51 +0200

    Commenting in Memory Operations and Cleanup for Get method

--- src/org/roboy/memory/util/MemoryOperations.java
+++ src/org/roboy/memory/util/MemoryOperations.java
@@ -7,11 +7,17 @@ import org.roboy.memory.models.*;
 import static org.roboy.memory.util.Answer.error;
 import static org.roboy.memory.util.Answer.ok;
 
+/**
+ * This class replicates the behaviour of ros.ServiceLogic. We make use of the exact same functions, just refactored to not be Services.
+ */
 public class MemoryOperations {
 
     private static Gson parser = new Gson();
-
-
+    /**
+     * Create a node
+     * @param request Query with data regarding the node. Ex: {"labels":["Organization"],"label":"Organization","properties":{"name":"korn"}}
+     * @return JSON containing the ID of the new node
+     */
     public static String create (String request){
         Create create = parser.fromJson(request, Create.class);
         if (create.validate()) {
@@ -25,21 +31,30 @@ public class MemoryOperations {
         }
     }
 
+    /**
+     * Get the Node ID
+     * @param request Query to specify Node to get. Ex: {"labels":["Person"],"label":"Person","properties":{"name":"davis"}}
+     * @return JSON containing ID of node
+     */
+    public static String get(String request){
+        Get get = parser.fromJson(request, Get.class);
 
-    public static String get(String json){
-        Get get = parser.fromJson(json, Get.class);
-
-        if(get.getId()!=null)
+        if(get.getId()!=null) {
             return Neo4j.getNodeById(get.getId());
-        else
+        }
+        else {
 //            if (! get.getLabel().equals("OTHER") )
-            if(get.getLabel() != "OTHER")
-
-                return Neo4j.getNode(get);
+            if (get.getLabel() != "OTHER") {return Neo4j.getNode(get);}
+        }
 
         return get.getError();
     }
 
+    /**
+     * Update Nodes
+     * @param request Query to link two nodes together. Ex: {"labels":["Person"],"label":"Person","properties":{"name":"davis"},"relationships":{"FROM":[369]},"id":368}
+     * @return JSON establishing whether or not the connection was made or not
+     */
     public static String update(String request) {
         Update update = parser.fromJson(request, Update.class);
 
@@ -54,6 +69,12 @@ public class MemoryOperations {
         }
     }
 
+    /**
+     * Cypher Method that is never called
+     * TODO: Implement this feature or refactor it out, it's kind of here because there was a service
+     * @param request
+     * @return
+     */
     public static String cypher(String request){
             return (Neo4j.run(request));
     }
@@ -67,6 +88,12 @@ public class MemoryOperations {
         return (Neo4j.run(request));
     }
 
+
+    /**
+     * Delete a Node
+     * @param request JSON query to delete a specified node. Ex: {'type':'node','id':361,'properties_list': ['sex'], 'relationships':{'FRIEND_OF':[426]}}
+     * @return Whether or not deleting was successful or not
+     */
     public static String remove(String request){
         Remove remove = parser.fromJson(request, Remove.class);
 
