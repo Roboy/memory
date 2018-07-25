1bbcbfa173906f9f05e5e4dafd6480c3ebfc0e0f
Jayce <luoyizhong98+git@gmail.com>
2018-07-25 03:15:06 +0200

    Auto stash before merge of "feature-logging" and "origin/feature-logging"

--- src/org/roboy/memory/util/MemoryLoggerInterface.java
+++ src/org/roboy/memory/util/MemoryLoggerInterface.java
@@ -5,21 +5,38 @@ import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
+
+/**
+ * An Interface for Dialog or other programs to set the Memory Modules Logger Level
+ *
+ * Since we are using jave.util.levels for Logging in memory, we shall use the levels specified here:
+ * https://docs.oracle.com/javase/8/docs/api/java/util/logging/Level.html
+ */
 public class MemoryLoggerInterface {
     private static Logger rootLogger = LogManager.getLogManager().getLogger("");
 
+    /**
+     * Sets level of Root and all Subhandlers to Level
+     * @param level Level to Set to
+     */
     public static void setLevel(Level level){
         rootLogger.setLevel(level);
         for (Handler h : rootLogger.getHandlers()) {
             h.setLevel(level);
         }
     }
-    public static void setLogger(String i) {
+
+    /**
+     * Sets level of Root and all Subhandlers to Level specified by a String
+     * If str is invalid, we shall default to INFO setting
+     * @param str
+     */
+    public static void setLogger(String str) {
         try {
-            Level l =Level.parse(i.toUpperCase());
+            Level l =Level.parse(str.toUpperCase());
             setLevel(l);
         }catch (RuntimeException e){
-            rootLogger.warning("Invalid Memory Logger Level Passed:\t" + i);
+            rootLogger.warning("Invalid Memory Logger Level Passed:\t" + str);
             rootLogger.warning("Please check Dialog's Config.Properties. Defaulting to INFO setting");
             setLevel(Level.INFO);
         }
