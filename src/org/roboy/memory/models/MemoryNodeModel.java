c2581195af38204f9787c6cc629b0fd31ba0c6ea
Wagram Airiian <vagram.airiian@tum.de>
2018-08-26 15:45:14 +0200

    Refactoring MemoryNodeModel. Extend NodeModel, move methods up to generalise and remove duplicates

--- src/org/roboy/memory/models/MemoryNodeModel.java
+++ src/org/roboy/memory/models/MemoryNodeModel.java
@@ -6,8 +6,13 @@ import com.google.gson.JsonElement;
 import com.google.gson.annotations.Expose;
 import com.google.gson.reflect.TypeToken;
 import org.roboy.memory.interfaces.Neo4jMemoryInterface;
+import org.roboy.memory.models.nodes.Interlocutor;
+import org.roboy.memory.util.DummyMemory;
+import org.roboy.ontology.Neo4jProperty;
+import org.roboy.ontology.Neo4jRelationship;
 import org.roboy.ontology.NodeModel;
 
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,9 +24,10 @@ import java.util.logging.Logger;
  * This class represents a full node similarly to its representation in Memory.
  */
 public class MemoryNodeModel extends NodeModel {
-    final private static Logger logger = Logger.getLogger(MemoryNodeModel.class.toString());
+    final private static Logger LOGGER = Logger.getLogger(MemoryNodeModel.class.toString());
     protected Neo4jMemoryInterface memory;
 
+    protected boolean initialized = false;
     protected boolean FAMILIAR = false;
     //If true, then fields with default values will be removed from JSON format.
     // Transient as stripping information is not a part of the node and not included in query.
@@ -34,7 +40,7 @@ public class MemoryNodeModel extends NodeModel {
     }
 
     public MemoryNodeModel(MemoryNodeModel node, Neo4jMemoryInterface memory){
-        super();
+        super(node);
         this.memory = memory;
         this.set(node);
     }
@@ -51,7 +57,105 @@ public class MemoryNodeModel extends NodeModel {
     }
 
     public boolean isFamiliar() {
+        if (!initialized) {
+            FAMILIAR = init(this);
+        }
         return FAMILIAR;
+    }
+
+    protected boolean init(MemoryNodeModel node) {
+        MemoryNodeModel result = this.queryForMatchingNodes(node);
+        initialized = true;
+        if (result != null) {
+            this.set(result);
+            return true;
+        } else {
+            result = this.create(node);
+            if (result != null) {
+                this.set(result);
+            }
+        }
+        return false;
+    }
+
+    private MemoryNodeModel queryForMatchingNodes(MemoryNodeModel node) {
+        if (node.getLabel() != null || node.getLabels() != null) {
+            ArrayList<Integer> ids = new ArrayList<>();
+            // Query memory for matching nodes.
+            try {
+                ids = memory.getByQuery(node);
+            } catch (InterruptedException | IOException e) {
+                LOGGER.info("Exception while querying memory, assuming node unknown.");
+                e.printStackTrace();
+            }
+            // Pick first if matches found.
+            if (ids != null && !ids.isEmpty()) {
+                //TODO Change from using first id to specifying if multiple matches are found.
+                try {
+                    return fromJSON(memory.getById(ids.get(0)), new Gson());
+                } catch (InterruptedException | IOException e) {
+                    LOGGER.warning("Unexpected memory error: provided ID not found upon querying.");
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+
+    private MemoryNodeModel create(MemoryNodeModel node) {
+        if(!(memory instanceof DummyMemory)) {
+            try {
+                int id = memory.create(node);
+                // Need to retrieve the created node by the id returned by memory
+                return fromJSON(memory.getById(id), new Gson());
+            } catch (InterruptedException | IOException e) {
+                LOGGER.warning("Unexpected memory error: provided ID not found upon querying.");
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Adds a new relationship to the node, updating memory.
+     */
+    public void addInformation(Neo4jRelationship relationship, String name) {
+        ArrayList<Integer> ids = new ArrayList<>();
+        // First check if node with given name exists by a matching query.
+        MemoryNodeModel relatedNode = new MemoryNodeModel(true, memory);
+        relatedNode.setProperties(Neo4jProperty.name, name);
+        //This adds a label type to the memory query depending on the relation.
+        relatedNode.setLabel(Neo4jRelationship.determineNodeType(relationship));
+        try {
+            ids = memory.getByQuery(relatedNode);
+        } catch (InterruptedException | IOException e) {
+            LOGGER.severe("Exception while querying memory by template.");
+            e.printStackTrace();
+        }
+        // Pick first from list if multiple matches found.
+        if(ids != null && !ids.isEmpty()) {
+            //TODO Change from using first id to specifying if multiple matches are found.
+            setRelationships(relationship, ids.get(0));
+        }
+        // Create new node if match is not found.
+        else {
+            try {
+                int id = memory.create(relatedNode);
+                if(id != 0) { // 0 is default value, returned if Memory response was FAIL.
+                    setRelationships(relationship, id);
+                }
+            } catch (InterruptedException | IOException e) {
+                LOGGER.severe("Unexpected memory error: creating node for new relation failed.");
+                e.printStackTrace();
+            }
+        }
+        //Update the person node in memory.
+        try{
+            memory.save(this);
+        } catch (InterruptedException | IOException e) {
+            LOGGER.severe("Unexpected memory error: updating person information failed.");
+            e.printStackTrace();
+        }
     }
 
     public void setStripQuery(boolean strip) {
